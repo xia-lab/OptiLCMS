1d8f31fabb42484830631400ea78a506d61a25da
Zhiqiang-PANG <zhiqiang.pang@mail.mcgill.ca>
2026-04-22 02:51:50 -0400

    fix warning issue

--- src/pense/nsoptim/armadillo_forward.hpp
+++ src/pense/nsoptim/armadillo_forward.hpp
@@ -18,6 +18,32 @@
 
 # include <RcppArmadillo/interface/RcppArmadilloForward.h>
 
+# ifndef Rcpp__exceptions_impl__h
+#  define Rcpp__exceptions_impl__h
+namespace Rcpp {
+inline void exception::record_stack_trace() {}
+inline void exception::copy_stack_trace_to_r() const { rcpp_set_stack_trace(R_NilValue); }
+}  // namespace Rcpp
+# endif
+
+# ifndef Rcpp_protection_meat_H
+#  define Rcpp_protection_meat_H
+namespace Rcpp {
+template <typename T>
+template <typename U>
+Armor<T>::Armor(U x) : data() {
+    init(wrap(x));
+}
+
+template <typename T>
+template <typename U>
+inline Armor<T>& Armor<T>::operator=(const U& x) {
+    REPROTECT(data = wrap(x), index);
+    return *this;
+}
+}  // namespace Rcpp
+# endif
+
 #ifdef __clang__
 #  pragma clang diagnostic pop
 #endif
